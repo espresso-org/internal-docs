9153efd95f285d86b2d96d75c0b4e731a81dc8f7
Maxime Cote <maxime.cote184@gmail.com>
2018-10-14 21:03:42 -0400

    Add usage and port parameter

--- dev-ipfs-server.sh
+++ dev-ipfs-server.sh
@@ -4,13 +4,26 @@
 # Installs and set up IPFS development server on port 64443
 #
 
+
+usage()
+{
+    echo "usage: dev-ipfs-server port "
+}
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 1
+fi
+
+
+
 export IPFS_PATH=/mnt/disks/ipfs-data
 wget https://dist.ipfs.io/go-ipfs/v0.4.17/go-ipfs_v0.4.17_linux-amd64.tar.gz
 tar xvfz go-ipfs_v0.4.17_linux-amd64.tar.gz
 cd go-ipfs
 ./install.sh
 ipfs init
-ipfs config Addresses.API /ip4/0.0.0.0/tcp/64443
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/$1
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 
 # Set up systemd daemon
@@ -23,7 +36,6 @@ After=syslog.target network.target remote-fs.target nss-lookup.target
 Type=simple
 Environment=IPFS_PATH=/mnt/disks/ipfs-data
 ExecStart=/usr/local/bin/ipfs daemon --enable-namesys-pubsub
-User=mathew_corm
 
 [Install]
 WantedBy=multi-user.target
@@ -31,5 +43,6 @@ EOT
 
 systemctl daemon-reload
 systemctl enable ipfs
+
 
 echo "Type 'sudo systemctl start ipfs' to start the IPFS daemon on port 64443"
