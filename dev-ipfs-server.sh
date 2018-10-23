ccbe29893fe4cc9df782bd56c84bba8ad7e4e153
Max Cote <maxime.cote184@gmail.com>
2018-10-23 12:09:38 -0400

    Add nginx reverse proxy

--- dev-ipfs-server.sh
+++ dev-ipfs-server.sh
@@ -23,7 +23,7 @@ tar xvfz go-ipfs_v0.4.17_linux-amd64.tar.gz
 cd go-ipfs
 ./install.sh
 ipfs init --profile server
-ipfs config Addresses.API /ip4/0.0.0.0/tcp/$1
+ipfs config Addresses.API /ip4/127.0.0.1/tcp/64442
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 
 # Set up systemd daemon
@@ -43,6 +43,37 @@ EOT
 
 systemctl daemon-reload
 systemctl enable ipfs
+
+
+# Nginx reverse proxy config
+
+apt install nginx
+systemctl start nginx
+systemctl enable nginx
+
+cat <<EOT >> /etc/nginx/conf.d/nginx.conf
+server {
+  listen $1;
+  listen [::]:$1;
+
+  #server_name example.com;
+
+  location / {
+      proxy_pass http://localhost:64442/;
+  }
+
+  # Blocked paths
+  location /api/v0/config/edit {}
+  location /api/v0/config/profile {}
+  location /api/v0/config/replace {}
+  location /api/v0/files/rm {}
+  location /api/v0/files/mv {}
+}
+EOT
+
+nginx -s reload
+
+###
 
 
 echo "Type 'sudo systemctl start ipfs' to start the IPFS daemon on port 64443"
